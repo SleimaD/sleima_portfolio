c6c2a10eb8d7dc336312dcc35c7f767a8fcf1b7c
SleimaD <sleima@Ordinateur-portable-de-Sleima.local>
2024-09-06 19:36:42 +0200

    add of pagination

--- app/works/page.jsx
+++ app/works/page.jsx
@@ -1,10 +1,8 @@
 "use client"
-import React from 'react'
-// import { bagelFatOne } from './../font'
+import React, {useState, useEffect} from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
 import worksData from './../json/works'
-// import { useRouter } from 'next/navigation'
 import {Bagel_Fat_One} from 'next/font/google'
 
 
@@ -16,22 +14,52 @@ export const bagelFatOne  = Bagel_Fat_One ({
 
 
 const Works = () => {
-    // const router = useRouter()
+
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(4);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1024) {
+        setItemsPerPage(2); 
+      } else {
+        setItemsPerPage(4); 
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const totalPages = Math.ceil(worksData.length / itemsPerPage);
+
+  const handleClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const currentItems = worksData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
  
-  return (
-    <div className={` relative text-white mt-[6rem] mb-16 `}>
-        <div className='flex justify-center w-full'>   
-            <div className={`p-2 flex gap-9 w-[90%] mb-[10rem] flex-wrap justify-center`}>
-                {worksData.map((element) => (  
-                    
-                                        
-                    <div key={element.alt}
-                    class="relative blockmauve flex h-[18rem] w-[28rem] items-center justify-center rounded-[1.5em] border-[1px] border-[#813d98dc] bg-[#813d982f] p-[0.5rem] text-lime-300 bg-opacity-75"
-                    >
-                    <div
-                        class="group absolute left-1/2 top-1/2 flex h-[3em] w-[3em] -translate-x-1/2 -translate-y-1/2 items-center justify-center overflow-hidden rounded-[1.5em] border-[1px] border-[#ffffffaa] bg-[#5453539a] backdrop-blur-[20px] duration-[500ms]  hover:h-[17rem] hover:w-[27em] max-[426px]:hover:w-[24.5em] max-[426px]:hover:h-[16rem]  blockhover hover:rounded-[1.5em]  "
-                    >
-                        <svg
+return (
+    <div className={`relative text-white mt-[0rem] mb-16`}>
+      <div className="flex justify-center w-full">
+        <div
+          className={`p-2 flex gap-9 w-[99%] mb-[1.5rem] flex-wrap justify-center`}
+        >
+          {currentItems.map((element) => (
+            <div
+              key={element.alt}
+              className="relative blockmauve flex h-[18rem] w-[28rem] items-center justify-center rounded-[1.5em] border-[1px] border-[#813d98dc] bg-[#813d982f] p-[0.5rem] text-lime-300 bg-opacity-75"
+            >
+              <div className="group absolute left-1/2 top-1/2 flex h-[3em] w-[3em] -translate-x-1/2 -translate-y-1/2 items-center justify-center overflow-hidden rounded-[1.5em] border-[1px] border-[#ffffffaa] bg-[#5453539a] backdrop-blur-[20px] duration-[500ms] hover:h-[17rem] hover:w-[27em] max-[426px]:hover:w-[24.5em] max-[426px]:hover:h-[16rem] blockhover hover:rounded-[1.5em]">
+               
+                <svg
                         className="h-[1.5em] w-[1.5em] duration-300 group-hover:opacity-0 cursor-pointer"
                         viewBox="0 0 48 48"
                         fill="none"
@@ -70,16 +98,37 @@ const Works = () => {
                         </div>
                             <a href={element.websiteUrl} className="cursor-pointer text-[1rem] underline"> View </a>
                         </div>
-                    </div>
-                    <img className=' w-full h-full  transition-all duration-300 rounded-xl ' src={element.image}  alt={element.alt} />   
-                    </div>
-                   
-                ))}
-                           
+
+
+              </div>
+              <img
+                className="w-full h-full transition-all duration-300 rounded-xl"
+                src={element.image}
+                alt={element.alt}
+              />
             </div>
-        </div>  
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center ">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i}
+            onClick={() => handleClick(i + 1)}
+            className={`mx-1 px-3 py-1 rounded ${
+              currentPage === i + 1
+                ? "bg-gray-500 text-white"
+                : "bg-transparent text-white"
+            } border border-white`}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </div>
-  )     
+  );
+
 }      
                                   
 export default Works;
